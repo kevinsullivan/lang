bfa4b166b48ef3a2b539e826bc9ce145d91023c6
Kevin Sullivan <sullivan.kevinj@gmail.com>
2020-08-08 18:52:11 +0000

    Added example showing environment override function used to define semantics of assignment.
    Co-authored-by: jll8uk <jll8uk@virginia.edu>

--- imperative_DSL/imperative.lean
+++ imperative_DSL/imperative.lean
@@ -17,13 +17,13 @@ mk :: (num : ℕ)
 inductive GeometricSpaceExpression
 | GeometricSpaceLiteral (V : geomSpace) : GeometricSpaceExpression
 | GeometricSpaceVariable (v : geomSpaceVar) : GeometricSpaceExpression
-| GeometricSpaceFunctionApp (V1 V2 : geomSpace) : GeometricSpaceExpression
+| GeometricProduct (V1 V2 : geomSpace) : GeometricSpaceExpression
 
 --Same for time spaces
 inductive TimeSpaceExpression
 | TimeSpaceLiteral (V : timeSpace) : TimeSpaceExpression
 | TimeSpaceVariable (v : timeSpaceVar) : TimeSpaceExpression
-| TimeSpaceFunctionApp (V1 V2 : timeSpace) : TimeSpaceExpression
+-- | TimeSpaceFunctionApp (V1 V2 : timeSpace) : TimeSpaceExpression
 
 --geometric Space Commands
 --a geomSpaceAssmt takes in a geom space variable, and a geom space expression
@@ -38,16 +38,18 @@ inductive timeSpaceCmd
 abbreviation geomSpaceEnvironment := (geomSpaceVar → geomSpace)
 abbreviation timeSpaceEnvironment := (timeSpaceVar → timeSpace)
 
+
+
 --Eval functions take in an expression, and an environment, and then returns a geomSpace
 def geomSpaceEval : GeometricSpaceExpression → geomSpaceEnvironment → geomSpace 
 | (GeometricSpaceExpression.GeometricSpaceLiteral V) E := V
 | (GeometricSpaceExpression.GeometricSpaceVariable v) E := E v
-| (GeometricSpaceExpression.GeometricSpaceFunctionApp V1 V2) E := V1 --not sure how to combine spaces yet
+| (GeometricSpaceExpression.GeometricProduct V1 V2) E := V1 --not sure how to combine spaces yet
 
 def timeSpaceEval : TimeSpaceExpression → timeSpaceEnvironment → timeSpace
 | (TimeSpaceExpression.TimeSpaceLiteral V) E := V
 | (TimeSpaceExpression.TimeSpaceVariable v) E := E v
-| (TimeSpaceExpression.TimeSpaceFunctionApp V1 V2) E := V1
+--| (TimeSpaceExpression.TimeSpaceFunctionApp V1 V2) E := V1
 
 --default environments
 def geomDefaultEnv : geomSpaceEnvironment := λ v, geomSpace.mk 3
@@ -72,3 +74,47 @@ def TimeSpaceCmd_eval : timeSpaceCmd → timeSpaceEnvironment → timeSpaceEnvir
         match var with
         | (timeSpaceVar.mk var_num) := timeSpaceEval e E
         end
+
+
+/- DEMO -/
+
+inductive bvar : Type
+| mk (n : ℕ)
+
+def bvar_eq : bvar → bvar → bool
+| (bvar.mk n1) (bvar.mk n2) := n1=n2
+
+inductive bExpr : Type
+|BLit (b: bool)
+|BVar (v: bvar)
+
+-- An environment is a function from bvar to bool
+
+def benv := bvar → bool
+
+def bEval : bExpr → benv → bool
+| (bExpr.BLit b) i := b
+| (bExpr.BVar v) i := i v
+
+
+def init_benv : benv := λ v, ff
+
+def update_benv : benv → bvar → bool → benv 
+| e v b := λ v2, if (bvar_eq v v2) then b else (e v2)
+
+inductive bCmd : Type
+| bAssm (v : bvar) (e : bExpr)
+| bSeq (c1 c2 : bCmd)
+
+def cEval : benv → bCmd → benv 
+| i0 c :=   match c with
+            | (bCmd.bAssm v e) := update_benv i0 v (bEval e i0)
+            | (bCmd.bSeq c1 c2) := 
+                let i1 := (cEval i0 c1) in (cEval i1 c2)
+            end
+
+def myFirstProg := bCmd.bAssm (bvar.mk 0) (bExpr.BLit ff)
+
+def newEnv := cEval init_benv myFirstProg
+
+#eval newEnv (bvar.mk 0)
