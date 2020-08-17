8fa9b6ec3d8fa0b6fc96085b6dd0dba6d24975a7
drewjel <44953238+drewjel@users.noreply.github.com>
2020-08-17 16:59:44 -0400

    Update physlang_test.lean

--- imperative_DSL/physlang_test.lean
+++ imperative_DSL/physlang_test.lean
@@ -1,16 +1,22 @@
 import .physlang
-
+import .environment
+import ..expressions.classical_geometry
+import ..expressions.classical_time
+import ..expressions.classical_velocity
 /-
 Test code
 -/
 def g1 := lang.classicalGeometry.var.mk 0
-def g2 := classicalGeometryVar.mk 1
+def g2 := lang.classicalGeometry.var.mk 1
 
 --default environments
-def geomDefaultEnv : classicalGeometryEnvironment := λ v, classicalGeometry.mk 0 3
-def timeDefaultEnv : classicalTimeEnvironment := λ v, classicalTime.mk 0
+def geomDefaultEnv : env := env.mk
+    (λ v, classicalGeometry.mk 0 3)
+    (λ v, classicalTime.mk 0)
+    (λ v, classicalVelocity.mk 0 (classicalGeometry.mk 0 3) (classicalTime.mk 0))
 
-def my_var : classicalGeometryVar := classicalGeometryVar.mk 0
-def myProgram : cmd := cmd.classicalGeometryAssmt my_var (classicalGeometryExpression.GeometricSpaceLiteral (Space.classicalGeometry.mk "" 3))
-#eval cmd_eval myProgram geomDefaultEnv
+
+def my_var : lang.classicalGeometry.var := lang.classicalGeometry.var.mk 0
+def myProgram : cmd := cmd.classicalGeometryAssmt my_var (lang.classicalGeometry.expr.lit (classicalGeometry.mk 0 3))
+#eval cmdEval myProgram geomDefaultEnv
  
