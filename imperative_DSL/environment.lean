d08ed1fb70a621f4700454ce2a5e3c38eb1f80af
drewjel <44953238+drewjel@users.noreply.github.com>
2020-08-17 16:50:33 -0400

    Update environment.lean

--- imperative_DSL/environment.lean
+++ imperative_DSL/environment.lean
@@ -24,3 +24,8 @@ def assignClassicalGeometry : env → lang.classicalGeometry.var → lang.classi
 | (env.mk g t v) var expr := let r := (lang.classicalGeometry.override g var expr) 
                              in (env.mk r t v)
 
+def assignClassicalTime : env → lang.classicalTime.var → lang.classicalTime.expr → env
+| (env.mk g t v) var expr := let r := (lang.classicalTime.override t var expr) in (env.mk g r v)
+
+def assignClassicalVelocity : env → lang.classicalVelocity.var → lang.classicalVelocity.expr → env 
+| (env.mk g t v) var expr := let r := (lang.classicalVelocity.override v var expr) in (env.mk g t r)
