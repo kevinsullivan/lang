65d572eaa976866486fd491d4fb9c05f616d5d01
drewjel <andrew.james.elsey@gmail.com>
2021-01-28 15:08:29 -0500

    Support for luminous intensity and for loops

--- imperative_DSL/environment.lean
+++ imperative_DSL/environment.lean
@@ -1,6 +1,7 @@
 import ..expressions.classical_geometry
 import ..expressions.classical_time
 import ..expressions.classical_hertz
+import ..expressions.classical_luminous_intensity
 --import ..expressions.classical_velocity
 import ..expressions.boolean
 import ..expressions.measurementSystem
@@ -15,6 +16,7 @@ mk ::   --(g: lang.classicalGeometry.spaceEnv)
         (t: lang.classicalTime.env)
         (g: lang.euclideanGeometry3.env)
         (h: lang.classicalHertz.env)
+        (li: lang.classicalLuminousIntensity.env)
         (ms: lang.measurementSystem.env)
         (ax : lang.axisOrientation.env)
         --(v: lang.classicalVelocity.env)
@@ -26,6 +28,7 @@ def init_env :env := --env.mk
                    lang.classicalTime.initEnv, 
                    lang.euclideanGeometry3.initEnv, 
                    lang.classicalHertz.initEnv,
+                   lang.classicalLuminousIntensity.initEnv,
                    lang.measurementSystem.initEnv,
                    lang.axisOrientation.initEnv
                    ⟩
--- imperative_DSL/physlang.lean
+++ imperative_DSL/physlang.lean
@@ -7,11 +7,13 @@ import ..expressions.measurementSystem
 import ..expressions.classical_geometry
 import ..expressions.classical_time
 import ..expressions.classical_hertz
+import ..expressions.classical_luminous_intensity
 --import ..expressions.classical_velocity
 --import ..expressions.classical_acceleration
 import ..override.geomOverride
 import ..override.timeOverride
 import ..override.hertzOverride
+import ..override.luminousIntensityOverride
 import ..override.measurementSystemOverride
 import ..override.axisOrientationOverride
 --import ..override.velocityOverride
@@ -112,6 +114,16 @@ inductive cmd : Type
     (e : lang.classicalHertz.QuantityExpr)
 | classicalHertzQuantityExpr
     (e : lang.classicalHertz.QuantityExpr)
+| classicalLuminousIntensityAssmt 
+    (v : lang.classicalLuminousIntensity.spaceVar) 
+    (e : lang.classicalLuminousIntensity.spaceExpr) 
+| classicalLuminousIntensityExpr
+    (e : lang.classicalLuminousIntensity.spaceExpr) 
+| classicalLuminousIntensityQuantityAssmt
+    (v : lang.classicalLuminousIntensity.QuantityVar)
+    (e : lang.classicalLuminousIntensity.QuantityExpr)
+| classicalLuminousIntensityQuantityExpr
+    (e : lang.classicalLuminousIntensity.QuantityExpr)
 | measurementSystemAssmt 
     (v : lang.measurementSystem.measureVar) 
     (e : lang.measurementSystem.measureExpr)
@@ -128,7 +140,12 @@ inductive cmd : Type
 | try_catch
     (t : cmd)
     (c : cmd)
-| if_then_else (b : bExpr) (t e : cmd)
+| if_then_else 
+    (b : bExpr) 
+    (t e : cmd)
+| for 
+    (b : bExpr) -- what goes here?
+    (d : cmd)
 | seq (c1 c2 : cmd)
 
 
@@ -136,6 +153,7 @@ notation one;two := cmd.seq one two
 notation; := cmd.skip
 notation `while `b,d := cmd.while b d
 notation `try `t,`catch `c := cmd.try_catch t c
+notation `for `b,d := cmd.for b d
 
 notation v=e := cmd.classicalTimeAssmt v e
 notation`expr `e := cmd.classicalTimeExpr e
@@ -171,6 +189,10 @@ notation v=e := cmd.classicalHertzAssmt v e
 notation`expr `e := cmd.classicalHertzExpr e
 notation v=e := cmd.classicalHertzQuantityAssmt v e
 notation `expr `e := cmd.classicalHertzQuantityExpr e
+notation v=e := cmd.classicalLuminousIntensityAssmt v e
+notation`expr `e := cmd.classicalLuminousIntensityExpr e
+notation v=e := cmd.classicalLuminousIntensityQuantityAssmt v e
+notation `expr `e := cmd.classicalLuminousIntensityQuantityExpr e
 notation v=e := cmd.measurementSystemAssmt v e
 notation v=e := cmd.axisOrientationAssmt v e
 notation `skip` := cmd.skip
@@ -216,12 +238,17 @@ def cmdEval : cmd → environment.env → environment.env
 | (classicalHertzExpr e) i := i 
 | (classicalHertzQuantityAssmt v e) i := assignHertzQuantity i v e
 | (classicalHertzQuantityExpr e) i := i
+| (classicalLuminousIntensityAssmt v e) i := assignLuminousIntensitySpace i v e
+| (classicalLuminousIntensityExpr e) i := i 
+| (classicalLuminousIntensityQuantityAssmt v e) i := assignLuminousIntensityQuantity i v e
+| (classicalLuminousIntensityQuantityExpr e) i := i
 | (measurementSystemAssmt v e) i := assignMeasurementSystem i v e
 | (measurementSystemExpr e) i := i
 | (axisOrientationAssmt v e) i := assignAxisOrientation i v e
 | (axisOrientationExpr e) i := i
 | (try_catch t c) i := i
 | (cmd.while b d) i := i--cmdEval d i -- this is not correct, but this is what it is for now.
+| (cmd.for b d) i := i
 | (cmd.if_then_else (b : bExpr)  (t : cmd) (f : cmd)) i := i --this is also incorrect? 1/21/20
     --if (bEval b bI) then (cmdEval t i) else (cmdEval f i) - BEN AND YANNI FILL IN --1/21/20 this is somewhat trickier
 | (seq (c1 : cmd) (c2 : cmd)) i0 := 
